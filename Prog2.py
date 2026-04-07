16d4459bd8ec18d3e3e3e9fe5b2d1188f880f210
yadavmk0001-sys <yadavmk0001@gmail.com>
2026-04-07 21:52:30 +0530

    Add files via upload

---
+++ Prog2.py
@@ -0,0 +1,13 @@
+Name = "Pragya"
+Age = 13
+is_student = True
+weight = 40
+
+print("Name : ", Name)
+print("Data Type of Name is ", type(Name))
+
+print("Age : ", Age)
+print("Type of Age is ", type(Age))
+
+print("is_student : ", is_student)
+print("Data Type of is_student is ", type(is_student))
